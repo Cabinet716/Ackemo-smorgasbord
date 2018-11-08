17a85a5315deaf069de3c7998459c978be5db9b3
William Ackemo <William.DTK.Ackemo@lystkom.se>
2018-11-08 14:01:14 +0100

    Follow lessons code

--- projects/javascript_101/assets/js/4-function.js
+++ projects/javascript_101/assets/js/4-function.js
@@ -54,3 +54,49 @@ function area(length, height) {
     console.log("The area of " + length + "and" + height + "is" + length + height);
 }
 area(5,10);
+
+function greet(person1, person2, person3, person4) {
+    console.log("Hej, ", person1 + "!");
+    console.log("Hej, ", person2 + "!");
+    console.log("Hej, ", person3 + "!");
+    console.log("Hej, ", person4 + "!");
+}
+
+greet("Adam", "PV", "Flimpflomp", "Doodis");
+
+function squared(x) {
+    return x * x;
+}
+
+console.log("4 squared is: ", squared(4));
+
+var result = squared(10);
+console.log(result);
+
+function isEven(x){
+    if (x % 2 == 0){
+    return true;
+    } else {
+        return false;
+    }
+}
+
+
+function kebabToSnake(str){
+    var myString = str.replace(/-/g, "_");
+    return myString;
+}
+
+function localScope(){
+    var showScope = 31;
+    console.log(showScope);
+}
+
+vae showScopeY = 99;
+function localScopeY(){
+    showScopeY = 100;
+    console.log(showScopeY);
+}
+
+console.log(showScopeY)
+localScopeY();
