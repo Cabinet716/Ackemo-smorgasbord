7df6ef8d4a3fafee0b39f421e956af40ee5eab53
William Ackemo <william.ackemo@hotmail.com>
2018-11-29 14:00:39 +0100

    created todo files

---
+++ projects/ToDo/assets/js/toDo.js
@@ -0,0 +1,4 @@
+// Create a ToDo application
+// Create an empty array
+// As long as the input is not "quit" the app should continue running
+// use while(), if() elseif(), forEach(), for(), push(), pop(), slice()
