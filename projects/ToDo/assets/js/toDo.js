07f57c3c7fa7db5ff6e8555aec41f8af7ddcf782
William Ackemo <william.ackemo@hotmail.com>
2018-11-30 11:13:31 +0100

    Tried something that didn't work...

--- projects/ToDo/assets/js/toDo.js
+++
@@ -1,4 +0,0 @@
-// Create a ToDo application
-// Create an empty array
-// As long as the input is not "quit" the app should continue running
-// use while(), if() elseif(), forEach(), for(), push(), pop(), slice()
