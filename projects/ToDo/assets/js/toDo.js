4511441bd81416f49083ca9265b76a2222f8d8ff
William Ackemo <william.ackemo@hotmail.com>
2018-12-06 13:53:22 +0100

    Completed

--- projects/ToDo/assets/js/toDo.js
+++ projects/ToDo/assets/js/toDo.js
@@ -1,28 +1,54 @@
-// Function to locally save the items in the todo list.
 function get_todos() {
-    // Creates a list in the div
-    var todos = new Array;
-    // localStorage.getItem(''); is used during the addTask function
-    var todos_str = localStorage.getItem('todo');
-    // If the local storage isn't empty, it will add the content in the 
+    var todos = new Array; // Creates a new array
+    var todos_str = localStorage.getItem('todo'); // Checks local story JSON todo
     if (todos_str !== null) {
-        todos = JSON.parse(todos_str); 
+        todos = JSON.parse(todos_str); // If JSON isn't empty, adds each string to the array
     }
     return todos;
 }
  
-function addTask() {
-    // Getting the input from the html
-    var task = document.getElementById('task').value;
-    // Pushing the input to local storage in a JSON
-    var todos = get_todos();
-    todos.push(task);
-    localStorage.setItem('todo', JSON.stringify(todos));
+function add() {
+    var task = document.getElementById('task').value; // Gets value from html input with Id task
+    
+    var todos = get_todos(); // Calls the list
+    todos.push(task); // Push the task to todos
+    localStorage.setItem('todo', JSON.stringify(todos)); // Saves list to JSON
  
     show();
  
     return false;
 }
-
-// function show()
-// function removeTask()
+ 
+function remove() {
+    var id = this.getAttribute('id'); // Creates a variable searching for elements with the attribute id
+    var todos = get_todos(); // Calls the list
+    todos.splice(id, 1); // Removes an element from the array
+    localStorage.setItem('todo', JSON.stringify(todos)); // Saves list to JSON
+ 
+    show();
+ 
+    return false;
+}
+ 
+function show() {
+    var todos = get_todos(); // Calls the list
+ 
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) 
+    {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">X</button></li>';
+        // Shows each item in list within the array from [0] -> [x] and adds a button with the content X inside
+    };
+    html += '</ul>';
+ 
+    document.getElementById('todos').innerHTML = html;
+ 
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) 
+    {
+        buttons[i].addEventListener('click', remove); // Adds the event of removing an element in the button
+    };
+}
+ 
+document.getElementById('add').addEventListener('click', add); // Adds the event of adding an element in the add button
+show();
