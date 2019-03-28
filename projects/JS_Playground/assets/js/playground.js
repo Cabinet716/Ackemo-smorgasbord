e13abaf75ce6d441cd92aefca064f8933833df0b
William Ackemo <william.ackemo@hotmail.com>
2019-03-28 19:09:58 +0100

    keept on working

--- projects/JS_Playground/assets/js/playground.js
+++ projects/JS_Playground/assets/js/playground.js
@@ -24,13 +24,13 @@ function drawFace(ctx, radius) {
 	ctx.fillStyle = 'white';
 	ctx.fill();
 	grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05); // it's a gradient on the outerside of the clock, but i kept to one colour
-	grad.addColorStop(1, 'black');
+	grad.addColorStop(1, 'grey');
 	ctx.strokeStyle = grad;
 	ctx.lineWidth = radius * 0.1;
 	ctx.stroke();
 	ctx.beginPath();
 	ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI); // small circle in the middle of the clock
-	ctx.fillStyle = 'black';
+	ctx.fillStyle = 'grey';
 	ctx.fill();
 }
 function drawNumbers(ctx, radius) {
@@ -79,4 +79,124 @@ function drawHand(ctx, pos, length, width) {
 	ctx.stroke();
 	ctx.rotate(-pos);
 }
+/*********************************************************************/
+
+/**********************Calculator*************************************/
+const calculator = {
+	displayValue: '0',
+	firstOperand: null,
+	waitingForSecondOperand: false,
+	operator: null
+};
+
+function inputDigit(digit) {
+	const { displayValue, waitingForSecondOperand } = calculator;
+
+	if (waitingForSecondOperand === true) {
+		calculator.displayValue = digit;
+		calculator.waitingForSecondOperand = false;
+	} else {
+		calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+	}
+}
+
+function inputDecimal(dot) {
+	if (calculator.waitingForSecondOperand === true) return;
+
+	// If the displayValue doesn't contain a decimal point
+	if (!calculator.displayValue.includes(dot)) {
+		// Append the decimal point
+		calculator.displayValue += dot;
+	}
+}
+
+function handleOperator(nextOperator) {
+	const { firstOperand, displayValue, operator } = calculator;
+	const inputValue = parseFloat(displayValue);
+
+	if (operator && calculator.waitingForSecondOperand) {
+		calculator.operator = nextOperator;
+		return;
+	}
+
+	if (firstOperand == null) {
+		calculator.firstOperand = inputValue;
+	} else if (operator) {
+		const currentValue = firstOperand || 0;
+		const result = performCalculation[operator](currentValue, inputValue);
+
+		calculator.displayValue = String(result);
+		calculator.firstOperand = result;
+	}
+
+	calculator.waitingForSecondOperand = true;
+	calculator.operator = nextOperator;
+}
+const performCalculation = {
+	'/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+	'*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+	'+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+	'-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+	'=': (firstOperand, secondOperand) => secondOperand
+};
+
+function resetCalculator() {
+	calculator.displayValue = '0';
+	calculator.firstOperand = null;
+	calculator.waitingForSecondOperand = false;
+	calculator.operator = null;
+}
+
+function updateDisplay() {
+	const display = document.querySelector('.calculator-screen');
+	display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+	const { target } = event;
+	if (!target.matches('button')) {
+		return;
+	}
+
+	if (target.classList.contains('operator')) {
+		handleOperator(target.value);
+		updateDisplay();
+		return;
+	}
+
+	if (target.classList.contains('decimal')) {
+		inputDecimal(target.value);
+		updateDisplay();
+		return;
+	}
+
+	if (target.classList.contains('clear')) {
+		resetCalculator();
+		updateDisplay();
+		return;
+	}
+
+	inputDigit(target.value);
+	updateDisplay();
+});
+/*********************************************************************/
+
+/********************************Darkmode*****************************/
+function toggleDarkLight() {
+	var body = document.getElementById('body');
+	var currentClass = body.className;
+	body.className = currentClass == 'dark-mode' ? 'light-mode' : 'dark-mode';
+}
+
+/*********************************************************************/
+
+/********************************Ballz********************************/
+
 /*********************************************************************/
