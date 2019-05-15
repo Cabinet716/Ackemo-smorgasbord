3dff24bb9129587a7350258e255ea8bbad70e7d2
William Ackemo <william.ackemo@hotmail.com>
2019-05-15 11:14:06 +0200

    Continued working

--- projects/JS_Playground/assets/js/playground.js
+++ projects/JS_Playground/assets/js/playground.js
@@ -78,23 +78,21 @@ function drawHand(ctx, pos, length, width) {
 /*********************************************************************/
 
 /**************************Lightbox**********************************/
-document.getElementById("clickme").addEventListener("click", function() {
-	document.getElementById("lightbox").className = "open";
-  });
-  
-  document.getElementById("close").addEventListener("click", function() {
-	document.getElementById("lightbox").className = "";
-  });
-  
-  document.getElementById("lightbox").addEventListener("click", function(e) {
-	if (e.target.id == "lightbox") {
-	  document.getElementById("lightbox").className = "";
+document.getElementById('clickme').addEventListener('click', function() {
+	document.getElementById('lightbox').className = 'open';
+});
+
+document.getElementById('close').addEventListener('click', function() {
+	document.getElementById('lightbox').className = '';
+});
+
+document.getElementById('lightbox').addEventListener('click', function(e) {
+	if (e.target.id == 'lightbox') {
+		document.getElementById('lightbox').className = '';
 	}
-  });
+});
 
 /********************************************************************/
-
-
 
 /**********************Calculator*************************************/
 const calculator = {
