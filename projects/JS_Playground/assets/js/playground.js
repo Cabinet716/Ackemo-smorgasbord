3cdabf36c32144d6b1fa59c2180e43ae54d10004
William Ackemo <william.ackemo@hotmail.com>
2019-05-24 15:17:39 +0200

    making progress

--- projects/JS_Playground/assets/js/playground.js
+++ projects/JS_Playground/assets/js/playground.js
@@ -210,7 +210,32 @@ function toggleDarkLight() {
 
 /*********************************************************************/
 
+/********************************Images*******************************/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+	showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName('pic');
+	if (n > x.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = x.length;
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = 'none';
+	}
+	x[slideIndex - 1].style.display = 'block';
+}
+/*********************************************************************/
+
 /********************************Shroom*******************************/
-/********************************Dunno********************************/
+var clicks = 0;
+var totClicks = 0;
 
 /*********************************************************************/
